84d0d27fd63e8708942e1b14f050da958d87c04a
Effiong Uyo <e.uyo@alustudent.com>
2026-09-10 13:59:13 +0200

    pushing webpack

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: [
+    './modules/header/header.js',
+    './modules/body/body.js',
+    './modules/footer/footer.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+  performance: {
+    hints: false,
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
